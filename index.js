0a921b1f4845f94028d61d1a5d8aa8eeb01efc5c
sulaiman-s <sulaimanfarooqi526@gmail.com>
2022-12-29 06:31:05 +0500

    up

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ const UpdateTrades = require("./startup/UpdatingTradesbackground");
 const express = require("express");
 const app = express();
 app.set("view engine", "pug");
-const { trans } = require("./utils/mailsend");
+
 require("./startup/routes")(app);
 require("./startup/db")();
 const port = process.env.Port || 4000;
 app.listen(port, () => {
   console.log("listening on port " + port);
-});
-
-trans.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
 });
 
 setInterval(() => {
--- utils/mailsend.js
+++ utils/mailsend.js
@@ -1,26 +1,69 @@
+// const { SMTPClient, Message } = require("emailjs");
 const config = require("config");
 const mail = require("nodemailer");
-const trans = mail.createTransport(config.get("mail"));
+const trans = mail.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "sulaimanfarooqi526@gmail.com",
+    pass: "idnuspckmcftqlxa",
+  },
+});
 
 function send(to, subject, text, verifyToken) {
   var mailOptions = {
-    from: config.get("mail.auth.user"),
+    from: "sulaimanfarooqi526@gmail.com",
     to: to,
     subject: subject,
+    // text: text,
     html: `
-    <div style=\" width:100%;height:100%; text-align:center;\"> 
-    <p>Please Verify Your Email by clicking the button below.</p></br>
-    <img src=\"https://mkdesigno.com/wp-content/uploads/2019/10/verify_email.png\" alt="logo" style=\" width:300px;height:300px; text-align:center;\"/></br>
-    <a style=\" max-width:200px; height:45px; padding:10px; border-radius:5px; border:1px solid white; color:white;background:blue;\" href=\"${config.get(
+<div style=\" width:100%;height:100%; text-align:center;\">
+<p>Please Verify Your Email by clicking the button below.</p></br>
+<img src=\"https://mkdesigno.com/wp-content/uploads/2019/10/verify_email.png\" alt="logo" style=\" width:300px;height:300px; text-align:center;\"/></br>
+<a style=\" max-width:200px; height:45px; padding:10px; border-radius:5px; border:1px solid white; color:white;background:blue;\" href=\"${config.get(
       "baseurl"
     )}/api/user/verify/${verifyToken}\">Verify Email</a>
-    </div>`,
+</div>`,
   };
   trans.sendMail(mailOptions, (error, info) => {
     if (error) console.log(error);
   });
 }
 
-// const html =
+// function send(to, subject, text, verifyToken) {
+//   const client = new SMTPClient({
+//     user: "sulaimanfarooqi526@gmail.com",
+//     password: "qypecazbmmwcquef",
+//     host: "smtp-mail.outlook.com",
+//     tls: {
+//       ciphers: "SSLv3",
+//     },
+//   });
 
-module.exports = { send, trans };
+//   const message = new Message({
+//     text: "i hope this works",
+//     from: "sulaimanfarooqi526@gmail.com",
+//     to: to,
+//     cc: "sulaimanfarooqi526@gmail.com",
+//     subject: subject,
+//     attachment: [
+//       {
+//         data: `<html><div style=\" width:100%;height:100%; text-align:center;\">
+//       <p>Please Verify Your Email by clicking the button below.</p></br>
+//       <img src=\"https://mkdesigno.com/wp-content/uploads/2019/10/verify_email.png\" alt="logo" style=\" width:300px;height:300px; text-align:center;\"/></br>
+//       <a style=\" max-width:200px; height:45px; padding:10px; border-radius:5px; border:1px solid white; color:white;background:blue;\" href=\"${config.get(
+//         "baseurl"
+//       )}/api/user/verify/${verifyToken}\">Verify Email</a>
+//       </div></html>`,
+//         alternative: true,
+//       },
+//     ],
+//   });
+
+//   client.send(message, function (err, message) {
+//     console.log(err || message);
+//   });
+// }
+
+module.exports = send;
