cda0360a82edb5d6c54c7826aba337ad2c3a36d7
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-01-13 23:37:18 +0500

    update:db

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,9 @@ const connection = require("../utils/connection");
 module.exports = function () {
   try {
     connection.authenticate().then(() => console.log("connected to database"));
-    connection.sync().then(() => console.log("synced successfully"));
+    connection
+      .sync({ alter: true })
+      .then(() => console.log("synced successfully"));
   } catch (error) {
     console.error("enable to connect to database");
   }
