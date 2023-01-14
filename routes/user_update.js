00d2f7628a4056421574f19837d93c81e013ad81
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-01-14 17:09:01 +0500

    update:user_profile

--- routes/user_update.js
+++ routes/user_update.js
@@ -105,11 +105,11 @@ const passValidate = (req) => {
 
 function validateFieldsToUpdate(req) {
   const schema = Joi.object({
-    user_name: Joi.string().optional().min(5).max(255),
-    first_name: Joi.string().optional(),
-    last_name: Joi.string().optional(),
-    contact: Joi.string().optional(),
-    email: Joi.string().email().optional(),
+    user_name: Joi.string().required().min(5).max(255),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    contact: Joi.string().required(),
+    email: Joi.string().email().required(),
   });
 
   return schema.validate(req);
